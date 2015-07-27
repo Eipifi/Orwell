968adb12f976a9f2898434b760445102cb578a2d
Michał Jabczyński <michal@jabczynski.net>
2015-07-28 01:15:18 +0200

    Exposed db transactions

--- lib/db/db_bolt.go
+++ lib/db/db_bolt.go
@@ -18,7 +18,7 @@ func open(path string) (r *BoltDB) {
     db, err := bolt.Open(path, 0600, nil)
     utils.Ensure(err)
     r = &BoltDB{db: db}
-    utils.Ensure(r.Update(initialize))
+    utils.Ensure(r.UpdateE(initialize))
     return
 }
 
@@ -38,93 +38,106 @@ func initialize(t *Tx) (err error) {
     return
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 func (b *BoltDB) Push(block *orchain.Block) error {
-    return b.Update(func(t *Tx) error {
+    return b.UpdateE(func(t *Tx) error {
         return t.PushBlock(block)
     })
 }
 
 func (b *BoltDB) Pop() {
-    b.Update(func(t *Tx) error {
+    b.Update(func(t *Tx) {
         t.PopBlock()
-        return nil
     })
 }
 
 func (b *BoltDB) State() (state *State) {
-    b.View(func(t *Tx) error {
+    b.View(func(t *Tx) {
         state = t.GetState()
-        return nil
     })
     return
 }
 
 func (b *BoltDB) Difficulty() (difficulty foo.U256) {
-    b.View(func(t *Tx) error {
+    b.View(func(t *Tx) {
         difficulty = t.Difficulty()
-        return nil
     })
     return
 }
 
 func (b *BoltDB) GetBlockByID(id foo.U256) (block *orchain.Block) {
-    b.View(func(t *Tx) error {
+    b.View(func(t *Tx) {
         block = t.GetBlock(id)
-        return nil
     })
     return
 }
 
 func (b *BoltDB) GetHeaderByNum(num uint64) (header *orchain.Header) {
-    b.View(func(t *Tx) error {
+    b.View(func(t *Tx) {
         header = t.GetHeaderByNum(num)
-        return nil
     })
     return
 }
 
 func (b *BoltDB) GetIDByNum(num uint64) (id *foo.U256) {
-    b.View(func(t *Tx) error {
+    b.View(func(t *Tx) {
         id = t.GetIDByNum(num)
-        return nil
     })
     return
 }
 
 func (b *BoltDB) GetNumByID(id foo.U256) (num *uint64) {
-    b.View(func(t *Tx) error {
+    b.View(func(t *Tx) {
         num = t.GetNumByID(id)
-        return nil
     })
     return
 }
 
 func (b *BoltDB) GetBills(wallet foo.U256) (res []orchain.BillNumber) {
-    b.View(func(t *Tx) error {
+    b.View(func(t *Tx) {
         res = t.GetUnspentBillsByUser(wallet)
-        return nil
     })
     return
 }
 
 func (b *BoltDB) GetBill(address orchain.BillNumber) (res *orchain.Bill) {
-    b.View(func(t *Tx) error {
+    b.View(func(t *Tx) {
         res = t.GetBill(&address)
-        return nil
     })
     return
 }
 
-func (b *BoltDB) Update(f func(*Tx) error) error {
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func (b *BoltDB) UpdateE(f func(*Tx) error) error {
     return b.db.Update(func(tx *bolt.Tx) error {
         return f(&Tx{tx})
     })
 }
 
-func (b *BoltDB) View(f func(*Tx) error) error {
+func (b *BoltDB) ViewE(f func(*Tx) error) error {
     return b.db.View(func(tx *bolt.Tx) error {
         return f(&Tx{tx})
     })
+}
+
+func (b *BoltDB) Update(f func(*Tx)) {
+    utils.Ensure(b.db.Update(func(tx *bolt.Tx) error {
+        f(&Tx{tx})
+        return nil
+    }))
+}
+
+func (b *BoltDB) View(f func(*Tx)) {
+    utils.Ensure(b.db.View(func(tx *bolt.Tx) error {
+        f(&Tx{tx})
+        return nil
+    }))
 }
 
 
@@ -132,9 +145,9 @@ func (b *BoltDB) View(f func(*Tx) error) error {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var instance DB
+var instance *BoltDB
 
-func Get() DB { return instance }
+func Get() *BoltDB { return instance }
 
 func Initialize(path string) {
     instance = open(path)
--- lib/wallet/api.go
+++ lib/wallet/api.go
@@ -3,6 +3,7 @@ import (
     "orwell/lib/foo"
     "orwell/lib/crypto/sig"
     "orwell/lib/utils"
+    "orwell/lib/db"
 )
 
 type Wallet struct {
@@ -13,4 +14,13 @@ func (w *Wallet) ID() foo.U256 {
     id, err := w.key.PublicPart().ID()
     utils.Ensure(err)
     return id
+}
+
+func (w *Wallet) Balance() (sum uint64) {
+    db.Get().View(func(t *db.Tx) {
+        for _, inp := range t.GetUnspentBillsByUser(w.ID()) {
+            sum += t.GetBill(&inp).Value
+        }
+    })
+    return
 }
